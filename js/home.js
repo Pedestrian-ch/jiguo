70a68064cd19e54f4cf7d6cbe1ec173dde6735c3
Pwotrot <1060910740@qq.com>
2020-10-23 13:11:42 +0800

    初步的文件结构

---
+++ js/home.js
@@ -0,0 +1,11 @@
+require.config({
+    paths:{
+        "jquery" : "jquery-3.5.1.js",
+        "slide" : "slide.js"
+    },
+    shim:{
+        "parabola" : {
+            exports : "_",
+        }
+    }
+})
