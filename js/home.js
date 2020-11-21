bda61510149eae4c398962be9838ff30e3e2167c
Pwotrot <1060910740@qq.com>
2020-11-21 09:45:48 +0800

    ......

--- js/home.js
+++ js/home.js
@@ -1,11 +1,15 @@
 require.config({
     paths:{
-        "jquery" : "jquery-3.5.1.js",
-        "slide" : "slide.js"
+        "jquery" : "jquery-3.5.1",
+        "slide" : "slide"
     },
     shim:{
         "parabola" : {
             exports : "_",
         }
     }
+})
+
+require(["slide"], function (slide){
+    slide.carousel();
 })
--- js/slide.js
+++ js/slide.js
@@ -1,3 +1,52 @@
-define(["jquery"],function (jquery){
-    
+define(["jquery"],function ($){
+    function carousel(){
+        $(".next").click(function (){
+            if(!$(".hot-slide .slide-box").is(":animated")){
+                $(".hot-slide .slide-box").animate({
+                    left: "-1000px",
+                },1000,function (){
+                    $(".slide-box > li").first().appendTo($(".slide-box"))
+                    $(".hot-slide .slide-box").css("left",0);
+                })
+            }
+        })
+
+        $(".prev").click(function (){
+            if(!$(".hot-slide .slide-box").is(":animated")){
+                $(".slide-box > li").last().prependTo($(".slide-box"))
+                $(".hot-slide .slide-box").css("left","-1000px");
+                $(".hot-slide .slide-box").animate({
+                left: 0,
+                },1000)
+            }
+        })
+
+        var t = setInterval(function (){
+            $(".hot-slide .slide-box").animate({
+                left: "-1000px",
+            },1000,function (){
+                $(".slide-box > li").first().appendTo($(".slide-box"))
+                $(".hot-slide .slide-box").css("left",0);
+            })
+        },3000)
+
+        $(".focus").mouseover(function (){
+            clearInterval(t);
+        })
+
+        $(".focus").mouseleave(function () { 
+            t = setInterval(function (){
+                $(".hot-slide .slide-box").animate({
+                    left: "-1000px",
+                },1000,function (){
+                    $(".slide-box > li").first().appendTo($(".slide-box"))
+                    $(".hot-slide .slide-box").css("left",0);
+                })
+            },3000)
+        });
+    }
+
+    return {
+        carousel:carousel,
+    }
 })
